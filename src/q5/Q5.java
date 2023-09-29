a3f8f49ffa9f35a47595e7111d87b8db04f31e90
juliaatwit <144470900+juliaatwit@users.noreply.github.com>
2023-09-29 12:17:27 -0400

    Add files via upload
    Initial commit

---
+++ src/q5/Q5.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package q5;
+
+/**
+ * 
+ */
+public class Q5 {
+
+	/**
+	 * 
+	 */
+	public Q5() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
